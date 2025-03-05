ce12df7de2b7ea7736593985debc5dd7becb9acd
LucasCarvalho01 <lucascarvalhopc@gmail.com>
2025-03-04 21:07:49 -0300

    mais endpoints

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req, Header, Param, Body, Query } from '@nestjs/common';
 import { Request } from 'express'
+import { CreateCatDto } from 'src/DTOs/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
@@ -9,4 +10,21 @@ export class CatsController {
   }
 
   
+  @Get()
+  async findAllWithQueryParams(@Query('age') age: number, @Query('breed') breed: string) {
+    return `This action returns all cats filtered by age: ${age} and breed: ${breed}`;
+  }
+
+  @Get(':id')
+  findOne(@Param() id: string): string {
+    console.log(id);
+    return `This action returns a #${id} cat`;
+}
+
+  @Post()
+  @Header('Cache-Control', 'no-store')
+  async create(@Body() CreateCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
 }
